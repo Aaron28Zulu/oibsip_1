b47aae0540e9c07ac4381c071d05bd1857ce3f55
Aaron <131490784+Aaron28Zulu@users.noreply.github.com>
2024-04-24 10:11:24 -0700

    Notes, wake up, and GUI features added

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+import tkinter as tk
+import calendar
 import speech_recognition as sr
 import pyttsx3
 import wikipedia as wk
-import time
-from gtts import gTTS
-import playsound
+import subprocess
+import threading
+from PIL import Image, ImageTk
 
 # TODO: Have the Mic active and listening to user' feed
 # TODO: Convert audio to text
@@ -21,16 +22,17 @@ import playsound
 # TODO: Trigger response to user' query 
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
+MONTHS = list(calendar.month_name)
+DAYS = list(calendar.day_name)
+DAY_EXTENSIONS = ["st", "nd", "rd"]
 
 def speak(text):
-    tts = gTTS(text="OK! " + text, lang="en", slow=False)
-    filename = "voice.mp3"
-    tts.save(filename)
-
-    playsound.playsound(filename)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
 
-def record_text():
+def record_audio():
     r = sr.Recognizer()
     with sr.Microphone() as mic:
         # Recognizer receive input 
@@ -38,35 +40,28 @@ def record_text():
         
         # Capture user voice
         audio = r.listen(mic)
-        myText = ""
+        r.adjust_for_ambient_noise(mic)
+        said = " "
+
         try:
-            myText = r.recognize_google(audio) # Service to recognize audio (i.e google)
+            said = r.recognize_google(audio) # Service to recognize audio (i.e google)
+            print(said)
         except sr.RequestError as e:
             print(f"Could not request results: {e}".format(e))
         except sr.UnknownValueError:
-            print("Unknown error occurred")
-    return myText
+            print("Unknown error")
+
+        return said.lower()
+
 
 def search_wiki(text):
-    while True:
-        try:
-            if "search" in text:
-
-                print("What should I look for...")
-
-                userText = "llama model"
-
-                results = wk.summary(userText, 2)
-                
-                print(results)
-
-
-                speak(results)
-
-                break
-
-        except Exception as e:
-            print("Exception error: " + str(e))
+    try:
+        results = wk.summary(text, 2)
+        print(results)
+        return results
+    except Exception as e:
+        print("Exception error: " + str(e))
+        icon.config(fg="black")
 
 
 def authenticate_calendar():
@@ -114,7 +109,86 @@ def get_event(num_of_events, service):
 
         print(start, event["summary"])
 
+service = authenticate_calendar() # To authenticate google calendar api
 
-service = authenticate_calendar()
 
-get_event(4, service=service)
+def get_date():
+    today = datetime.date.today()
+    return today
+
+
+def write_note(text):
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(file_name, "w") as file:
+        file.write(text)
+
+    subprocess.Popen(["notepad.exe", file_name])
+
+
+def main():
+    isBot_of = False
+    WAKE = ["hey bot", "bot", "wake", "wake up"]
+
+    while not isBot_of:
+        print("Listening...")
+        text = record_audio()
+
+        if text is not None:
+            icon.config(fg="green")
+            
+        else:
+            icon.config(fg="black")
+
+        # WAKE UP BOT
+        for word in WAKE: 
+            if word in text:
+                speak("Hi, I'm Bot. How can I assist you?")
+        
+                text = record_audio() # Reinitilise mic to get new input
+
+        # TAKES NOTES
+        NOTE_STRS = ["make a note", "write this down", "remember this", "take notes"]
+        for phrase in NOTE_STRS:
+            if phrase in text:
+                speak("What would you like me to note down?")
+                note_text = record_audio()
+                write_note(note_text)
+                speak("I've made a note of that.")
+
+        # WIKI SEARCH
+        WIKIPEDIA_STRS = ["search", "wikipedia", "give insight", "summerize", "summerise"]
+        for word in WIKIPEDIA_STRS:
+            if word in text:
+                speak("What would you like me to summerize?")
+                text = record_audio()
+
+                summarised_note = search_wiki(text)
+                speak(summarised_note)
+
+        # DATES
+        DATE_STRS = ["what is the date today"]
+        for phrase in DATE_STRS:
+            if phrase in text:
+                date = get_date()
+                speak(date)
+
+#GUI
+root = tk.Tk()
+root.title("Bot Assistant")
+root.minsize(width=300, height=300)
+root.resizable(False, False)
+
+
+gui_msg = tk.Message(root, text="Try saying", justify="center", pady=30)
+gui_msg.grid(column=1, row=0)
+
+icon = tk.Label(root, text="🔊", font=("Arial", 50, "bold"))
+icon.grid(column=0, row=1)
+
+msg = tk.Message(root, text="tell me a joke", justify="center")
+msg.grid(column=1, row=1)
+
+threading.Thread(target=main).start()
+
+root.mainloop()
